JacobTanenbaum/ovn

commit 2d2e1e600fd04a6ca7aa60da9c28b2fb12f5b3a1

util: Improve dynamic_bitmap APIs.
Introduce some missing functions to dynamic_bitmap APIs.

Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

lib/ovn-util.h
@@ -502,6 +502,7 @@ void sorted_array_apply_diff(const struct sorted_array *a1,
                                                     bool add),
                              const void *arg);
 
+/* */
 struct dynamic_bitmap {
     unsigned long *map;
     size_t n_elems;
@@ -562,11 +563,23 @@ dynamic_bitmap_get_n_elems(const struct dynamic_bitmap *db)
     return db->n_elems;
 }
 
+static inline bool
+dynamic_bitmap_is_set(const struct dynamic_bitmap *db, size_t offset)
+{
+    return bitmap_is_set(db->map, offset);
+}
+
+static inline size_t
+dynamic_bitmap_count1(const struct dynamic_bitmap *db)
+{
+    return bitmap_count1(db->map, db->capacity);
+}
+
 static inline void
 dynamic_bitmap_set1(struct dynamic_bitmap *db, int index)
 {
     ovs_assert(index < db->capacity);
-    if (!bitmap_is_set(db->map, index)) {
+    if (!dynamic_bitmap_is_set(db, index)) {
         bitmap_set1(db->map, index);
         db->n_elems++;
     }
@@ -576,10 +589,18 @@ static inline void
 dynamic_bitmap_set0(struct dynamic_bitmap *db, int index)
 {
     ovs_assert(index < db->capacity);
-    if (bitmap_is_set(db->map, index)) {
+    if (dynamic_bitmap_is_set(db, index)) {
         bitmap_set0(db->map, index);
         db->n_elems--;
     }
+}
+
+static inline void
+dynamic_bitmap_or(struct dynamic_bitmap *db,
+                  const unsigned long *arg, size_t n)
+{
+    ovs_assert(db->capacity == n);
+    bitmap_or(db->map, arg, n);
 }
 
 static inline unsigned long *

commit bd185b0ae48c15f049f4d77659b55638c7073b5c

lib: Fix n_elems accounting in dynamic_bitmap_or().
Performing dynamic_bitmap_or() can change the number of elements,
causing n_elems to potentially not reflect the number of elements in the
bitmap. Recalculate the n_elems in dynamic_bitmap_or().

Fixes: 2d2e1e600fd0 ("util: Improve dynamic_bitmap APIs.")
Assisted-by: Claude Opus 4.8, Claude Code
Signed-off-by: Jacob Tanenbaum <jtanenba@redhat.com>

lib/ovn-util.h
@@ -640,6 +640,7 @@ dynamic_bitmap_or(struct dynamic_bitmap *db,
 {
     ovs_assert(db->capacity == n);
     bitmap_or(db->map, arg, n);
+    db->n_elems = bitmap_count1(db->map, db->capacity);
 }
 
 static inline unsigned long *

tests/ovn.at
@@ -2458,6 +2458,7 @@ AT_CLEANUP
 AT_SETUP([Sparse array operations])
 check ovstest test-sparse-array add
 check ovstest test-sparse-array remove-replace
+check ovstest test-sparse-array bitmap-or
 AT_CLEANUP
 
 AT_SETUP([SPSC ring buffer])

tests/test-sparse-array.c
@@ -189,13 +189,61 @@ test_remove_replace(struct ovs_cmdl_context *ctx OVS_UNUSED)
 }
 
 static void
+test_dynamic_bitmap_or(struct ovs_cmdl_context *ctx OVS_UNUSED)
+{
+    struct dynamic_bitmap a, b;
+
+    dynamic_bitmap_alloc(&a, 8);
+    dynamic_bitmap_alloc(&b, 8);
+
+    /* Set bits 1, 3 in 'a' through the tracked API. */
+    dynamic_bitmap_set1(&a, 1);
+    dynamic_bitmap_set1(&a, 3);
+    ovs_assert(a.n_elems == 2);
+
+    /* Set bits 3, 5, 7 in 'b'. */
+    dynamic_bitmap_set1(&b, 3);
+    dynamic_bitmap_set1(&b, 5);
+    dynamic_bitmap_set1(&b, 7);
+    ovs_assert(b.n_elems == 3);
+
+    /* OR 'b' into 'a'.  Result should be {1, 3, 5, 7} = 4 elements.
+     * Before the fix, n_elems stayed at 2 because dynamic_bitmap_or
+     * did not recount. */
+    dynamic_bitmap_or(&a, b.map, b.capacity);
+    ovs_assert(a.n_elems == 4);
+    ovs_assert(dynamic_bitmap_is_set(&a, 1));
+    ovs_assert(dynamic_bitmap_is_set(&a, 3));
+    ovs_assert(dynamic_bitmap_is_set(&a, 5));
+    ovs_assert(dynamic_bitmap_is_set(&a, 7));
+    ovs_assert(!dynamic_bitmap_is_set(&a, 0));
+    ovs_assert(!dynamic_bitmap_is_set(&a, 2));
+
+    /* Clearing a bit that was added by the OR must not underflow
+     * n_elems.  Before the fix, n_elems was 2 here so clearing two
+     * OR-added bits would wrap to SIZE_MAX. */
+    dynamic_bitmap_set0(&a, 5);
+    ovs_assert(a.n_elems == 3);
+    dynamic_bitmap_set0(&a, 7);
+    ovs_assert(a.n_elems == 2);
+    dynamic_bitmap_set0(&a, 1);
+    ovs_assert(a.n_elems == 1);
+    dynamic_bitmap_set0(&a, 3);
+    ovs_assert(a.n_elems == 0);
+
+    dynamic_bitmap_free(&a);
+    dynamic_bitmap_free(&b);
+}
+
+static void
 test_sparse_array_main(int argc OVS_UNUSED, char *argv[] OVS_UNUSED)
 {
     ovn_set_program_name(argv[0]);
     static const struct ovs_cmdl_command commands[] = {
-        {"add",            NULL, 0, 0, test_add,            OVS_RO},
-        {"remove-replace", NULL, 0, 0, test_remove_replace, OVS_RO},
-        {NULL,             NULL, 0, 0, NULL,                OVS_RO},
+        {"add",            NULL, 0, 0, test_add,              OVS_RO},
+        {"remove-replace", NULL, 0, 0, test_remove_replace,  OVS_RO},
+        {"bitmap-or",      NULL, 0, 0, test_dynamic_bitmap_or, OVS_RO},
+        {NULL,             NULL, 0, 0, NULL,                 OVS_RO},
     };
     struct ovs_cmdl_context ctx;
     ctx.argc = argc - 1;